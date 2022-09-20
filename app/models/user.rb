74e611cd94093a968205dd5c7545a771c937cb09
silvia pasini <silvia.pasini03@gmail.com>
2022-09-20 12:50:56 +0200

    routes, seeds, controllers cretion

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reviews, dependent: :destroy
-  has_many :article, dependent: :destroy
+  has_many :cars, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 
 end
